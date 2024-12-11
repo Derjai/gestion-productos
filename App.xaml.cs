6e890643b6feeb2233dc1dfbbe0c0579f7228839
Derjai <ojaider@uninorte.edu.co>
2024-12-11 15:10:36 -0500

    Conexión a Base de datos: - Añadida cadena de conexión - Añadida Clase Modelo - Añadida Clase para poblar y probar base de datos

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using gestion_productos.Data;
+using System;
 using System.Windows;
 
 namespace gestion_productos
 {
-    /// <summary>
-    /// Lógica de interacción para App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            using (var context = new InventarioDBContext())
+            {
+                DataSeeder.Seed(context);
+            }
+
+        }
     }
 }
